8842edc70aa8eea7a922546655fca4e02fc53dec
Nidhi Kulkarni <nkulkarni@mux.com>
2020-03-10 14:47:25 -0700

    cloud functions

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,33 @@
-Parse.Cloud.define('hello', function(req, res) {
-  return 'Hi';
+const Mux = require('@mux/mux-node');
+const uuid = require('uuid/v1');
+
+// This assumes you have MUX_TOKEN_ID and MUX_TOKEN_SECRET 
+// environment variables.
+const { Video } = new Mux();
+
+
+Parse.Cloud.define('upload', function(req, res) {
+  const id = uuid();
+  
+  // Create a new upload using the Mux SDK.
+  const upload = await Video.Uploads.create({
+    // Set the CORS origin to your application.
+    cors_origin: 'https://tiktokclone.herokuapp.com',
+
+    // Specify the settings used to create the new Asset after
+    // the upload is complete
+    new_asset_settings: {
+      passthrough: id,
+      playback_policy: 'public',
+    }
+  });
+  var Post = Parse.Object.extend({
+    className: "Post"
+  });
+  post.set("uploadId", upload.id);
+  post.set("metadata", assetInfo);
+  post.set("status", 'waiting_for_upload');
+  
+   // Now send back that ID and the upload URL so the client can use it!
+  return { id, url: upload.url };
 });
